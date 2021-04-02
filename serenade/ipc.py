60342084a2325de5cc3dd52489dbe5c512ec75bb
Tommy MacWilliam <tommy@serenade.ai>
2021-04-01 22:04:46 -0700

    remove code that's no longer needed
    remove shell integrations and code paths that will be handled by the
    Serenade app.

--- serenade/ipc.py
+++ serenade/ipc.py
@@ -8,32 +8,22 @@ class Ipc:
     def __init__(self, command_handler):
         self.command_handler = command_handler
         self.websocket = None
-
         self.app = "iterm"
         self.plugin_id = str(uuid.uuid4())
-        self.active_message = {
-            "app": self.app,
-            "id": self.plugin_id
-        }
+        self.active_message = {"app": self.app, "id": self.plugin_id}
         self.url = "ws://localhost:17373/"
-
         self.heartbeat_task = asyncio.create_task(self.heartbeat())
 
     async def retry_connection(self):
         while True:
             # Reset the active/heartbeat message with a new UUID
             self.plugin_id = str(uuid.uuid4())
-            self.active_message = {
-                "app": self.app,
-                "id": self.plugin_id
-            }
+            self.active_message = {"app": self.app, "id": self.plugin_id}
             try:
                 await self.connect()
             except (OSError, websockets.exceptions.ConnectionClosedError):
                 self.websocket = None
-                # print("Could not connect")
                 await asyncio.sleep(1)
-                # print ("Retrying ...")
 
     async def connect(self):
         async with websockets.connect(self.url) as websocket:
@@ -52,12 +42,7 @@ class Ipc:
 
     async def send(self, message, data):
         if self.websocket:
-            # print("Sending raw message:")
-            # print(message, data)
-            await self.websocket.send(json.dumps({
-                "message": message,
-                "data": data
-            }))
+            await self.websocket.send(json.dumps({"message": message, "data": data}))
 
     async def message_handler(self):
         async for message in self.websocket:
@@ -71,9 +56,4 @@ class Ipc:
             callback = data.get("callback")
             result = await self.command_handler.handle(data.get("response"))
 
-            await self.send("callback", {
-                "callback": callback,
-                "data": result
-            })
-            # print("Received raw message:")
-            # print(message)
+            await self.send("callback", {"callback": callback, "data": result})
--- uninstall.sh
+++ uninstall.sh
@@ -2,20 +2,12 @@
 
 set -e
 
-if [[ $SHELL == *"bash"* ]]; then
-  echo "Removing ~/.serenade/iterm2/bin/serenade-shell-integration.bash from ~/.bash_profile"
-  sed -i '' 's/source ~\/.serenade\/iterm2\/bin\/serenade-shell-integration.bash//g' ~/.bash_profile
-elif [[ $SHELL == *"zsh"* ]]; then
-  echo "Removing ~/.serenade/iterm2/bin/serenade-shell-integration.zsh from ~/.zshrc"
-  sed -i -e 's/source ~\/.serenade\/iterm2\/bin\/serenade-shell-integration.zsh//g' ~/.zshrc
-fi
-
-dest="$HOME/Library/Application Support/iTerm2/Scripts"
+destination="$HOME/Library/Application Support/iTerm2/Scripts"
 
 echo "Removing symlinks from:"
-echo "$dest"
-rm -rf "$dest"/serenade
-rm -f "$dest"/AutoLaunch.scpt
+echo "$destination"
+rm -rf "$destination"/serenade
+rm -f "$destination"/AutoLaunch.scpt
 
 echo "Removing ~/.serenade/iterm2"
 rm -rf ~/.serenade/iterm2
