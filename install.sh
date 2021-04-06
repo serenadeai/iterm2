e3107ddb5fd02ff45945af766a02686359506573
Tommy MacWilliam <tommy@serenade.ai>
2021-04-06 15:12:37 -0700

    use HTTPS git URL for install script

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@
 set -e
 
 echo "Cloning git@github.com:serenadeai/iterm2.git to ~/.serenade/iterm2"
-git clone --depth 1 git@github.com:serenadeai/iterm2.git ~/.serenade/iterm2
+git clone --depth 1 https://github.com/serenadeai/iterm2.git ~/.serenade/iterm2
 
 directory="$HOME/.serenade/iterm2"
 destination="$HOME/Library/Application Support/iTerm2/Scripts"
